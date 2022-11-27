d5ddb32abfb814bfab8437e92a9e99a719e12799
DanteDercksen <Ruan@draco.co.za>
2022-11-27 18:33:03 +0200

    NPC's Added with Animations
    Animations can be switched using a static function with parameter of type enum such as (Idle, Talking)

---
+++ Assets/Scripts/NPC/NPCAnim.cs
@@ -0,0 +1,51 @@
+using System;using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class NPCAnim : MonoBehaviour
+{
+    private static NPCState _currentState;
+
+    private static Animator _control;
+
+    private static string idle = "IsIdle", talking = "IsTalking";
+
+    private void Start()
+    {
+        _control = GetComponent<Animator>();
+        SwitchState(_currentState);
+    }
+
+    //For Debugging Purposes
+    [ContextMenu("Switch State")]
+    private void Control()
+    {
+        SwitchState(_currentState);
+    }
+
+    public static void SwitchState(NPCState newState)
+    {
+        _currentState = newState;
+        
+        if (_currentState == NPCState.IDLE)
+        {
+            _control.SetBool(idle, true);
+            _control.SetBool(talking, false);
+            return;
+        }
+
+        if (_currentState == NPCState.TALKING)
+        {
+            _control.SetBool(idle, false);
+            _control.SetBool(talking, true);
+            return;
+        }
+    }
+}
+
+public enum NPCState
+{
+    IDLE,
+    TALKING
+}
