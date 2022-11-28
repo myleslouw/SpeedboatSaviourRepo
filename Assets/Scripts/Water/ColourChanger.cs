f5a3c02f516cf68b16c2a9c30ea102479e1b7da7
myleslouw <56117169+myleslouw@users.noreply.github.com>
2022-11-28 15:56:38 +0200

    colour changing works

--- Assets/Scripts/Water/ColourChanger.cs
+++ Assets/Scripts/Water/ColourChanger.cs
@@ -7,20 +7,21 @@ public class ColourChanger : MonoBehaviour
 {
     public MeshRenderer Mesh;
     Color ShallowStartColour = new Color(0.081f, 0.281f, 0.459f, 0.247f);
-    Color DeepStartColour = new Color(0.000f, 0.533f, 0.035f, 0.537f);
+    Color DeepStartColour = new Color(0.006f, 0.236f, 0.021f, 0.537f);
 
-    Color ShallowMidColour = new Color(0.081f, 0.281f, 0.459f, 0.247f);
-    Color DeepMidColour = new Color(0.000f, 0.533f, 0.401f, 0.537f);
+    Color Shallow2ndColour = new Color(0.081f, 0.281f, 0.459f, 0.247f);
+    Color Deep2ndColour = new Color(0.013f, 0.387f, 0.032f, 0.537f);
+
+    Color Shallow3rdColour = new Color(0.081f, 0.281f, 0.459f, 0.247f);
+    Color Deep3rdColour = new Color(0.177f, 0.462f, 0.352f, 0.537f);
+
+    Color Shallow4thColour = new Color(0.081f, 0.281f, 0.459f, 0.247f);
+    Color Deep4thColour = new Color(0.033f, 0.408f, 0.472f, 0.537f);
 
     Color ShallowEndColour = new Color(0.118f, 0.925f, 0.840f, 0.808f);
-    Color DeepEndColour = new Color(0.087f, 0.452f, 0.594f, 0.694f);
+    Color DeepEndColour = new Color(0.027f, 0.564f, 0.811f, 0.537f);
 
     private Animal_WanderScript wanderScript;
-
-    private void Awake()
-    {
-        
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,12 +38,6 @@ public class ColourChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Mesh.material.SetColor("ShallowWater", ShallowEndColour);
-            Mesh.material.SetColor("DeepWater", DeepEndColour);
-        }
 
         if (Input.GetKeyDown(KeyCode.P))
         {
@@ -64,8 +59,16 @@ public class ColourChanger : MonoBehaviour
                 Mesh.material.SetColor("DeepWater", DeepStartColour);
                 break;
             case 1:
-                Mesh.material.SetColor("ShallowWater", ShallowMidColour);
-                Mesh.material.SetColor("DeepWater", DeepMidColour);
+                Mesh.material.SetColor("ShallowWater", Shallow2ndColour);
+                Mesh.material.SetColor("DeepWater", Deep2ndColour);
+                break;
+            case 2:
+                Mesh.material.SetColor("ShallowWater", Shallow3rdColour);
+                Mesh.material.SetColor("DeepWater", Deep3rdColour);
+                break;
+            case 4:
+                Mesh.material.SetColor("ShallowWater", Shallow4thColour);
+                Mesh.material.SetColor("DeepWater", Deep4thColour);
                 break;
             default:
                 Mesh.material.SetColor("ShallowWater", ShallowEndColour);
